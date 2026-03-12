2db1dacea188252d66095d4dcdbd71500c0c24b9
LRS-Coder <robert.larini@outlook.com>
2026-03-12 21:10:53 +0000

    added a counter
    counter keeps track of the number of guesses the user has made and states this number after each guess.

--- Week1/debugging_exercise.py
+++ Week1/debugging_exercise.py
@@ -3,16 +3,21 @@ import random
 # pick a random number for the user to guess
 rand = random.randint(1, 20)
 
+# adds a counter to keep track of number of guesses
+count = 0
+
 print('Guess a number between 1 and 20.')
 guess = int(input())  # number needs to be an integer
+count += 1
 
 while guess != rand:  # if the guess is not equal to the random number, you have to guess again
     if guess > rand:  # if the guess is too high, tell the user.
-        print('Too high. Guess again.')
+        print('Too high. Guess again. Number of guesses is {}.'.format(count))
     else:  # if the guess is too low, tell the user.
-        print('Too low. Guess again.')
+        print('Too low. Guess again. Number of guesses is {}.'.format(count))
 
     print('Enter a new guess: ')
     guess = int(input())
+    count += 1
 
-print('You got it! The number was {}'.format(rand))
+print('You got it in {} guesses! The number was {}'.format(count, rand))
